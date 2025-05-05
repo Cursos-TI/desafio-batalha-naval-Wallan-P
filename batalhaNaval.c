50fdb07edade06efd6dab19defbbcf4a1f749c16
Wallan-P <wallanbezerra@gmail.com>
2025-05-05 16:53:31 +0000

    Nivel: Mestre Status: Concluido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,127 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int conef(int cone[][5]){
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    printf("cone\n");
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    for (int i = 0; i < 3; i++){
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+        for (int j = 0; j < 5; j++){
+        
+            
+            if (i==2 || (i==1 && j<4 && j>0) || i==0 && j==2  ){
+                cone[i][j]=5;
+            };
+        }
+    }
 }
+
+int cruzf(int cruz[][5]){
+
+    printf("\ncruz\n");
+
+    for (int i = 0; i < 3; i++){
+
+        for (int j = 0; j < 5; j++){
+        
+            
+            if (j==2 ||i==1  ){
+                cruz[i][j]=5;
+            };
+        }
+    }
+}
+
+int octaedrof(int octaedro[][5]){
+
+    printf("\noctaedro\n");
+
+    for (int i = 0; i < 3; i++){
+
+        for (int j = 0; j < 5; j++){
+        
+            
+            if (j==2 ||i==1 && j>0 && j<4  ){
+                octaedro[i][j]=5;
+            };
+        }
+    }
+}
+
+
+
+
+int main(){
+
+int cone[3][5]={0};
+int cruz[3][5]={0};
+int octaedro[3][5]={0};
+int tabuleiro[10][10] = {0};
+
+conef(cone);
+cruzf(cruz);
+octaedrof(octaedro);
+printf("\n");
+
+
+for (int i = 0; i < 10; i++){
+
+    for (int j = 0; j < 10; j++){
+    
+        if (i==7 && j==7) {
+
+            tabuleiro[i][j]=3;
+            tabuleiro[i][j+1]=3;
+            tabuleiro[i][j+2]=3;
+    
+    
+           };
+         if (i==3 && j==4) {
+    
+            tabuleiro[i][j]=3;
+            tabuleiro[i+1][j]=3;
+            tabuleiro[i+2][j]=3;
+    
+    
+       };
+    
+         if (i==0 && j==7) {
+    
+            tabuleiro[i][j]=3;
+            tabuleiro[i+1][j+1]=3;
+            tabuleiro[i+2][j+2]=3;
+    
+    
+       };
+    
+         if (i==7 && j==2) {
+    
+            tabuleiro[i][j]=3;
+            tabuleiro[i+1][j+1]=3;
+            tabuleiro[i+2][j+2]=3;
+        };
+    
+      
+        if(i<3 && j<5) {tabuleiro[i][j] = cone[i][j];} 
+
+        if(i>=3 && i<=5 && j>=5 && j<=9) {tabuleiro[i][j] = cruz[i-3][j-5];} 
+
+        if(i>=6 && i<=8 && j<=5) {tabuleiro[i][j] = octaedro[i-6][j-1];} 
+
+        
+
+ 
+        printf("%d ",tabuleiro[i][j]);
+
+    }
+    printf("\n");
+}
+printf("\n\n\n");
+
+
+
+return 0;
+};
+
+
+ 
